55c6fe577cd49025d78ab0d473ff6c7ba01671ac
Genieliu <feixiangljs@gmail.com>
2015-01-02 21:16:22 +0800

    Binary Search Tree iterator

---
+++ Binary_Search_Tree_Iterator.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+
+    public BSTIterator(TreeNode root) {
+        
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
