42c7613f9e6451279fd0840b415f1209e92aaa96
Genieliu <feixiangljs@gmail.com>
2015-01-03 21:43:43 +0800

    Binary_Search_Tree_Iterator

--- Binary_Search_Tree_Iterator.java
+++ Binary_Search_Tree_Iterator.java
@@ -7,21 +7,36 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+import java.util.*;
 
 public class BSTIterator {
 
+    Stack<TreeNode> seque;
+
     public BSTIterator(TreeNode root) {
-        
+        seque = new Stack<>();
+        if(root != null)seque.push(root);
     }
 
-    /** @return whether we have a next smallest number */
+    //* @return whether we have a next smallest number
     public boolean hasNext() {
-        
+        return !seque.empty();
     }
 
     /** @return the next smallest number */
     public int next() {
-        
+
+        TreeNode x = seque.pop();
+        if(x.left == null) {
+            if(x.right != null) seque.push(x.right);
+            return x.val;
+        } else {
+            seque.push(x);
+            seque.push(x.left);
+            x.left = null;
+            return next();
+        }
+
     }
 }
 
